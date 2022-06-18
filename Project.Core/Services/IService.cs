935d00dbd91cd651f3608c4914a38292f24fccfc
Oğuzhan <58344612+okutucu@users.noreply.github.com>
2022-06-18 12:51:13 +0300

    Added Generic repository

--- Project.Core/Services/IService.cs
+++ Project.Core/Services/IService.cs
@@ -8,15 +8,13 @@ namespace Project.Core.Services
         Task<T> GetByIdAsync(int id);
         Task<IEnumerable<T>> GetAllAsync();
         IQueryable<T> Where(Expression<Func<T, bool>> exp);
-        IQueryable<T> FirstOrDefault(Expression<Func<T, bool>> exp);
         Task<bool> AnyAsync(Expression<Func<T, bool>> exp);
         Task AddAsync(T entity);
         Task AddRangeAsync(IEnumerable<T> entities);
         Task UpdateAsync(T entity);
-        Task UpdateRangeAsync(T entity);
+
         Task RemoveAsync(T entity);
         Task RemoveRangeAsync(IEnumerable<T> entities);
-        Task Destroy(T entity);
-        Task DestroyRange(IEnumerable<T> entities);
+  
     }
 }
---
+++ Project.Repository/Repositories/GenericRepository.cs
@@ -0,0 +1,67 @@
+﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using Project.Core.Models;
+using Project.Core.Repositories;
+
+namespace Project.Repository.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
+    {
+        protected readonly AppDbContext _context;
+
+        private readonly DbSet<T> _dbSet;
+
+        public GenericRepository(AppDbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+
+        public async Task AddAsync(T entity)
+        {
+           await _dbSet.AddAsync(entity);
+        }
+
+        public async Task AddRangeAsync(IEnumerable<T> entities)
+        {
+            await _dbSet.AddRangeAsync(entities);
+        }
+
+        public async Task<bool> AnyAsync(Expression<Func<T, bool>> exp)
+        {
+            return await _dbSet.AnyAsync(exp);
+        }
+
+
+
+        public IQueryable<T> GetAll(Expression<Func<T, bool>> exp)
+        {
+            return _dbSet.AsNoTracking().AsQueryable();
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public void Remove(T entity)
+        {
+            _dbSet.Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<T> entities)
+        {
+           _dbSet.RemoveRange(entities);
+        }
+
+        public void Update(T entity)
+        {
+            _dbSet.Update(entity);
+        }
+
+        public IQueryable<T> Where(Expression<Func<T, bool>> exp)
+        {
+            return _dbSet.Where(exp);
+        }
+    }
+}
