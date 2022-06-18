4c7d86f4d83757126389c5fbd57f23371438fef4
Oğuzhan <58344612+okutucu@users.noreply.github.com>
2022-06-18 11:05:48 +0300

    added Service Repository

---
+++ Project.Core/Services/IService.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq.Expressions;
+using Project.Core.Models;
+
+namespace Project.Core.Services
+{
+    public interface IService<T> where T : BaseEntity
+    {
+        Task<T> GetByIdAsync(int id);
+        Task<IEnumerable<T>> GetAllAsync();
+        IQueryable<T> Where(Expression<Func<T, bool>> exp);
+        Task<bool> AnyAsync(Expression<Func<T, bool>> exp);
+        Task AddAsync(T entity);
+        Task AddRangeAsync(IEnumerable<T> entities);
+        Task UpdateAsync(T entity);
+        Task RemoveAsync(T entity);
+        Task RemoveRangeAsync(IEnumerable<T> entities);
+    }
+}
