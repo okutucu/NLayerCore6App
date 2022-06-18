9ba4e0cd65e2f7d590d3f7bf45e8a9fea389212f
Oğuzhan <58344612+okutucu@users.noreply.github.com>
2022-06-18 10:58:11 +0300

    Added Generic Repository

---
+++ Project.Core/Repositories/IGenericRepository.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq.Expressions;
+using Project.Core.Models;
+
+namespace Project.Core.Repositories
+{
+    public interface IGenericRepository<T> where T : BaseEntity
+    {
+        Task<T> GetByIdAsync(int id);
+        IQueryable<T> GetAll(Expression<Func<T, bool>> exp);
+        // IQueryable<T> kullandığımız için ;
+        // productRepository.Where(x=> x.id>5) -> buraya kadar daha veri tabanına gitmedi.
+        // productRepository.Where(x=> x.id>5).OrderBy.ToListAsync() dediğimiz anda dbye sorgu yapacağı için daha performanslı olacaktır.
+        IQueryable<T> Where(Expression<Func<T,bool>> exp);
+        Task<bool> AnyAsync(Expression<Func<T, bool>> exp);
+        Task AddAsync(T entity);
+        Task AddRangeAsync(IEnumerable<T> entities);
+        void Update(T entity);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entities);
+
+
+
+    }
+}
