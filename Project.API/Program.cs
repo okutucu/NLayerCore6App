bb8e4f8c40dd1d0537a542f2568f345a4e4e4df4
Oğuzhan <58344612+okutucu@users.noreply.github.com>
2022-06-22 11:45:54 +0300

    Add Fluent Validation

--- Project.API/Program.cs
+++ Project.API/Program.cs
@@ -1,4 +1,5 @@
 using System.Reflection;
+using FluentValidation.AspNetCore;
 using Microsoft.EntityFrameworkCore;
 using Project.Core.Repositories;
 using Project.Core.Services;
@@ -8,12 +9,13 @@ using Project.Repository.Repositories;
 using Project.Repository.UnitOfWorks;
 using Project.Service.Mapping;
 using Project.Service.Services;
+using Project.Service.Validations;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddFluentValidation(x => x.RegisterValidatorsFromAssemblyContaining<ProductDtoValidator>());
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
