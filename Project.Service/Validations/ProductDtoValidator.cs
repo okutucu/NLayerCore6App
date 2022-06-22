297ca2c41d60d5433e52ad150ac63132d9de3548
Oğuzhan <58344612+okutucu@users.noreply.github.com>
2022-06-22 12:10:19 +0300

    ..

--- Project.Service/Validations/ProductDtoValidator.cs
+++ Project.Service/Validations/ProductDtoValidator.cs
@@ -13,9 +13,9 @@ namespace Project.Service.Validations
         public ProductDtoValidator()
         {
             RuleFor(x => x.Name).NotNull().WithMessage("{PropertyName} is required").NotEmpty().WithMessage("{PropertyName} is required");
-            RuleFor(x => x.Price).InclusiveBetween(1, int.MaxValue).WithMessage("{PropertyName} must be grater then 0");
-            RuleFor(x => x.Stock).InclusiveBetween(1, int.MaxValue).WithMessage("{PropertyName} must be grater then 0");
-            RuleFor(x => x.CategoryId).InclusiveBetween(1, int.MaxValue).WithMessage("{PropertyName} must be grater then 0");
+            RuleFor(x => x.Price).InclusiveBetween(1, int.MaxValue).WithMessage("{PropertyName} must be greater then 0");
+            RuleFor(x => x.Stock).InclusiveBetween(1, int.MaxValue).WithMessage("{PropertyName} must be greater then 0");
+            RuleFor(x => x.CategoryId).InclusiveBetween(1, int.MaxValue).WithMessage("{PropertyName} must be greater then 0");
         }
     }
 }
